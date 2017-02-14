17c6aec7a8bc46780d7b0d7276db79d2cc7821eb
Jean Ureña <juanpedro1519@gmail.com>
2017-02-14 01:43:08 -0300

    Models, Migrations and DBAPI

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,3 +3,28 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# models
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	# human readable name
+	pub_date = models.DateTimeField('date published')
+
+	# to printing beautiful in console
+	def __str__(self):
+		return self.question_text
+
+	# custom method reutrn false / true
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+	# foreign key
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	# default = 0
+	votes = models.IntegerField(default=0)
+
+	# to printing beautiful in console
+	def __str__(self):
+		return self.choice_text
