5a28318271107b1e2a121a440a5dad25f176cfe5
Jean Ureña <juanpedro1519@gmail.com>
2017-02-15 00:45:31 -0300

    Templates and More Views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,19 @@ from django.conf.urls import url
 
 from . import views
 
+# namespace to differientate when call {% url %} betweeen apps
+app_name = "polls"
+
+# url to view matching
 urlpatterns = [
+	# ex /polls/
 	url(r'^$', views.index, name='index'),
+	# ex /polls/5
+	# ?P<question_id> -> name of te parameter
+	# the 'name' value as called by the {% url %} template tag
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	# ex /polls/5/results
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	# ex /polls/5/vote
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World!. You are at polls index")
+	# gettip top 5 lattest post
+	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+	#output = ", ".join([q.question_text for q in latest_question_list])
+	# render template
+	context = {
+		"latest_question_list" : latest_question_list,
+	}
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	# long form
+	# try:
+	# 	question = Question.objects.get(pk=question_id)
+	# except Question.DoesNotExits:
+	# 	# 404 Error
+	# 	raise Http404("Question does not exit")
+
+	# 404 shorcut if the record was not found
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+	response = "You're Looking at the results of the Question %s"
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You are voting on question %s" % question_id)
