23dc31d50e372d655dca26554e18a3586a8de3ad
Jean Ureña <juanpedro1519@gmail.com>
2017-02-14 23:40:08 -0300

    Django Admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Question
 # Register your models here.
+
+# register to the admin
+admin.site.register(Question)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
 # models
+@python_2_unicode_compatible # support for python 2
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	# human readable name
@@ -18,6 +20,7 @@ class Question(models.Model):
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+@python_2_unicode_compatible # support for python 2
 class Choice(models.Model):
 	# foreign key
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
