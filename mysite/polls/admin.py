6506e8133f1989d85b8ad615315f89fe2a561f13
Jean Ureña <juanpedro1519@gmail.com>
2017-02-18 15:19:48 -0300

    Modifying the Django Admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
+# to add choice options from Question
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	Extra = 3
+
+# modifying admin
+class QuestionAdmin(admin.ModelAdmin):
+	# fielsets - > divide the form in sections
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information' , {'fields': ["pub_date"],
+		# collapse
+		'classes' : ['collapse']})
+	]
+	# adding Choices to Questions
+	inlines = [ChoiceInline]
+	# changing the Select question to change fields
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	# adds a "Filter" sidebar that lets people filter the change 
+	# list by the pub_date field:
+	list_filter = ['pub_date']
+	# search capability filted by question_text
+	search_fields = ['question_text']
+
+
 # register to the admin
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
